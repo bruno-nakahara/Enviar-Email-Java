04a5fbb59cd630a385de3e36520a4e5ba30d2fc5
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-10-25 23:17:23 -0300

    add simulador de pdf e enviar email com pdf anexado

--- sending-email/src/main/java/sending/email/ObjectSendEmail.java
+++ sending-email/src/main/java/sending/email/ObjectSendEmail.java
@@ -1,23 +1,38 @@
 package sending.email;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
 import javax.mail.Address;
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class ObjectSendEmail {
 
-	private String userName = "XXX@gmail.com";
-	private String password = "XXX";
+	private String userName = "xxx@gmail.com";
+	private String password = "xxx";
 	
 	private String destinationList = "";
 	private String nomeRemetente = "";
@@ -32,7 +47,7 @@ public class ObjectSendEmail {
 		
 	}
 	
-	public void SendEmail() throws MessagingException, UnsupportedEncodingException {
+	public void SendEmail(boolean sendHtml) throws MessagingException, UnsupportedEncodingException {
 		
 		Properties properties = new Properties();
 		
@@ -58,10 +73,90 @@ public class ObjectSendEmail {
 		message.setFrom(new InternetAddress(userName, nomeRemetente));//transmissor
 		message.setRecipients(Message.RecipientType.TO, toUser);//destino
 		message.setSubject(emailTitle);//Assunto
-		message.setText(emailText);
+		
+		if (sendHtml) {
+			message.setContent(emailText, "text/html; charset=utf-8");
+		} else {
+			message.setText(emailText);
+		}
 		
 		Transport.send(message);
+	
+	}
+	
+	/* Método Simulador de PDF anexado ao email enviado*/
+	private FileInputStream simuladorDePDF() throws Exception{
+		Document document = new Document();
+		File file = new File("anexo.pdf");
+		file.createNewFile();
+		PdfWriter.getInstance(document, new FileOutputStream(file));
+		document.open();
+		document.add(new Paragraph("Conteudo do PDF anexo com Java Mail"));
+		document.close();
 		
-		System.out.println(session);
+		return new FileInputStream(file);
+	}
+	
+	public void SendEmailAttachment (boolean sendHtml) throws IOException, Exception {
+		
+		Properties properties = new Properties();
+		
+		properties.put("mail.smtp.ssl.trust", "*");
+		properties.put("mail.smtp.auth", "true");//Autorização
+		properties.put("mail.smtp.starttls", "true");//Autenticação
+		properties.put("mail.smtp.host", "smtp.gmail.com");//Servidor gmail
+		properties.put("mail.smtp.port", "465");//Porta do servidor
+		properties.put("mail.smtp.socketFactory.port", "465");//Especifica a porta a ser conectada pelo socket
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");//Classe socket de conexão ao SMTP
+		
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				
+				return new PasswordAuthentication(userName, password);
+			}
+		});
+		
+		Address[] toUser = InternetAddress.parse(destinationList); 
+		
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(userName, nomeRemetente));//transmissor
+		message.setRecipients(Message.RecipientType.TO, toUser);//destino
+		message.setSubject(emailTitle);//Assunto
+		
+		/*Texto e a descrição do email*/
+		MimeBodyPart emailBody = new MimeBodyPart();
+		
+		if (sendHtml) {
+			emailBody.setContent(emailText, "text/html; charset=utf-8");
+		} else {
+			emailBody.setText(emailText);
+		}
+		
+		List<FileInputStream> files = new ArrayList<FileInputStream>();
+		files.add(simuladorDePDF());
+		files.add(simuladorDePDF());
+		files.add(simuladorDePDF());
+		
+		Multipart multipart = new MimeMultipart();
+		multipart.addBodyPart(emailBody);
+		
+		int index = 0;
+		for (FileInputStream fileInputStream : files) {
+			/*Anexo em pdf*/
+			MimeBodyPart emailAttachment = new MimeBodyPart();
+			
+			emailAttachment.setDataHandler(new DataHandler(new ByteArrayDataSource(fileInputStream, "application/pdf")));
+			emailAttachment.setFileName("emailAttachment" + index + ".pdf");
+			
+			multipart.addBodyPart(emailAttachment);
+			
+			index++;
+		}
+		
+		message.setContent(multipart);
+		
+		Transport.send(message);
+	
 	}
 }
--- sending-email/src/test/java/sending/email/AppTest.java
+++ sending-email/src/test/java/sending/email/AppTest.java
@@ -2,6 +2,7 @@ package sending.email;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
@@ -21,17 +22,23 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest 
-{
-	
-	
+{	
 	@org.junit.Test
-    public void testEmail() throws UnsupportedEncodingException, MessagingException {
-    	 ObjectSendEmail sendEmail = new ObjectSendEmail("XXX@gmail.com",
+    public void testEmail() throws IOException, Exception {
+		
+		 StringBuilder stringBuilderTextEmail = new StringBuilder();
+		
+		 stringBuilderTextEmail.append("Olá, <br/><br/>");
+		 stringBuilderTextEmail.append("Você está recebendo um email no formato HTML <br/><br/>");
+		 stringBuilderTextEmail.append("Para ter acesso ao curso, clique no botão abaixo. <br/><br/>");
+		 stringBuilderTextEmail.append("<a target=\"_blank\" href=\"https://www.projetojavaweb.com/certificado-aluno/plataforma-curso/aula?codigoCurso=1\" style=\"color:#2525a7;\">Link para o Curso</a> <br/><br/>");
+		
+    	 ObjectSendEmail sendEmail = new ObjectSendEmail("xxx@gmail.com",
     			 										"JDev Treinamentos",
     			 										"Estudando sobre Java",
-    			 										"Testando envio de email utilizando Java");
+    			 										stringBuilderTextEmail.toString());
     	 
-    	 sendEmail.SendEmail();
+    	 sendEmail.SendEmailAttachment(true);
 		
 		
 	}
